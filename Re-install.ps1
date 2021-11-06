1cdae72dc3d87c825541ad0294413e26969c205a
Kalen <kalenarndt@gmail.com>
2021-11-06 13:19:24 -0600

    Update Re-install.ps1

--- Re-install.ps1
+++ Re-install.ps1
@@ -1,6 +1,6 @@
 
 Param(  
-    [string]$chocolateyAppList = "firefox,vlc,google-drive-file-stream,lavfilters7zip,wsl2,steam,obsidian,packer,vault,putty,obs-studio,zoom,fluent-terminal,signal,github,terraform,vagrant,discord,telegram,powertoys,ccleaner,slack,vscode,awscli,spotify,vcredist140,vcredist2013,vcredist2008,vcredist2012,vcredist2010",
+    [string]$chocolateyAppList = "vlc,brave,google-drive-file-stream,lavfilters7zip,wsl2,steam,obsidian,packer,vault,obs-studio,zoom,signal,github,terraform,vagrant,discord,telegram,slack,vscode,awscli,spotify,vcredist140,vcredist2013,vcredist2008,vcredist2012,vcredist2010",
     [string]$desktop = $false,
     [string]$nvidia = $true
     # [string]$dismAppList = "Microsoft-Windows-Subsystem-Linux, VirtualMachinePlatform"
@@ -30,7 +30,9 @@ if ([string]($desktop) -eq $true){
 
 if ([string]($nvidia) -eq $true){   
     Write-Host "Nvidia Option Specified, Installing Drivers"  
-    choco install nvidia-display-driver /y | Write-Output
+    choco feature enable -n=useRememberedArgumentsForUpgrades
+    choco install nvidia-display-driver --package-parameters="'/dch'" /y | Write-Output
+    
 }
 
 
@@ -46,15 +48,3 @@ if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false){
         & choco install $app /y | Write-Output
     }
 }
-
-# if ([string]::IsNullOrWhiteSpace($dismAppList) -eq $false){
-#     Write-Host "DISM Features Specified"    
-
-#     $appsToInstall = $dismAppList -split "," | ForEach-Object { "$($_.Trim())" }
-
-#     foreach ($app in $appsToInstall)
-#     {
-#         Write-Host "Installing $app"
-#         & choco install $app /y /source windowsfeatures | Write-Output
-#     }
-# }
